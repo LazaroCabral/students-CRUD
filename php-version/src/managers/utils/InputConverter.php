c9e11c396b758ecd905d8c299fd3c0b83002d9dc
Lazaro Cabral <lazarocabralamro@gmail.com>
2024-07-18 22:58:12 -0300

    conversion by reference

--- php-version/src/managers/utils/InputConverter.php
+++ php-version/src/managers/utils/InputConverter.php
@@ -4,10 +4,12 @@ namespace Lazaro\StudentCrud\Managers\Utils;
 
 class InputConverter{
 
-    static function statusConveter($status): int{
+    static function statusConveter(&$status): int{
         if($status == "on" || $status == "true"){
+            $status=1;
             return 1;
         }
+        $status=0;
         return 0;
     }
 }
